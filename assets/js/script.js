c1d38755e8b37a3552aceb1d198ed2b04df9bf9f
Q4zerr <avet.tanguy1STMG2@gmail.com>
2024-03-06 17:21:19 +0100

    Ajout d'animations et page reservation

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,74 @@ burgerMenu.addEventListener("click", () => {
   burgerMenu.classList.toggle("active");
   menuMobile.classList.toggle("active");
 });
+
+/*Change link*/
+const links = document.querySelectorAll(".menu-item");
+const linkStats = document.querySelector(".menu-item.stats");
+const linkReservation = document.querySelector(".menu-item.reservation");
+const statsPart = document.querySelector(".statistics");
+const reservationPart = document.querySelector(".reservation-part");
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    links.forEach((link) => {
+      link.classList.remove("active");
+      link.classList.add("disabled");
+    });
+    if (link.classList.contains("disabled")) {
+      link.classList.remove("disabled");
+      link.classList.add("active");
+    }
+    if (link.classList.contains("reservation")) {
+      statsPart.classList.add("hidden");
+      reservationPart.classList.remove("hidden");
+    } else {
+      statsPart.classList.remove("hidden");
+      reservationPart.classList.add("hidden");
+    }
+  });
+});
+
+/*Change color of table*/
+const tables = document.querySelectorAll(".table-map button");
+
+tables.forEach((table) => {
+  table.addEventListener("click", () => {
+    if (table.classList.contains("red")) {
+      table.classList.remove("red");
+      table.classList.add("green");
+    } else {
+      table.classList.remove("green");
+      table.classList.add("red");
+    }
+  });
+});
+
+/*Change color of room*/
+const rooms = document.querySelectorAll(".room-map .available .square");
+rooms.forEach((room) => {
+  room.addEventListener("click", () => {
+    if (room.classList.contains("red")) {
+      room.classList.remove("red");
+      room.classList.add("green");
+    } else {
+      room.classList.remove("green");
+      room.classList.add("red");
+    }
+  });
+});
+
+/*Switch map reservation*/
+const dropDown = document.querySelector(".dropdown-select .dropdown");
+const tableMap = document.querySelector(".table-map");
+const roomMap = document.querySelector(".room-map");
+
+dropDown.addEventListener("change", () => {
+  if (dropDown.value == "table") {
+    tableMap.classList.remove("disabled");
+    roomMap.classList.add("disabled");
+  } else {
+    roomMap.classList.remove("disabled");
+    tableMap.classList.add("disabled");
+  }
+});
