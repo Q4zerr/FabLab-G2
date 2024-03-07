79c23625ffa2f057e58fe737ab978d85cca34a42
Luca Améaume <62576432+LuluxClient@users.noreply.github.com>
2024-03-07 20:27:22 +0100

    Code cleanup & fix lots of bugs

--- assets/php/function.inc.php
+++ assets/php/function.inc.php
@@ -11,24 +11,6 @@
         return $response_data;
     }
 
-    // function getStatsDay(){
-    //     $currentHour = getCurrentHour();
-    //     $hoursOfDay = ['8h', '9h', '10h', '11h', '12h', '13h', '14h', '15h', '16h', '17h', '18h', '19h'];
-
-    //     // Parcours des heures du jours
-    //     foreach ($hoursOfDay as $hour) {
-    //         $randomLevel = getRandomValue();
-    //         // On ajoute la classe "actual" à l'heure actuel
-    //         $chartClass = ($hour === $currentHour) ? 'actual' : '';
-
-    //         echo '<div class="chart ' . $chartClass . '">';
-    //         echo '  <div class="chart-level" style="height: ' . $randomLevel . '%"></div>';
-    //         echo '  <div class="chart-day">' . $hour . '</div>';
-    //         echo '</div>';
-    //     }
-    // }
-
-
     function getStatsDay($jour, $campus_id) {
         $currentHour = getCurrentHour();
         $currentDay = getCurrentDay();
@@ -48,14 +30,7 @@
             echo '</div>';
         }
     }
-    
-    function getActualValueFromDatabase($jour, $heure, $campus_id) {
-        $api_url = "http://45.13.119.138:8000/frequence/jour/$jour/heure/$heure/campus/$campus_id";
-        $json_data = file_get_contents($api_url);
-        $response_data = json_decode($json_data);
-    
-        return isset($response_data->frequence_etudiant) ? $response_data->frequence_etudiant : 0;
-    }
+
 
     function getCurrentDay() {
         $currentDate = date('Y-m-d');
@@ -79,15 +54,6 @@
     
         $currentHour = date('H');
         return $currentHour . 'h';
-    }
-
-    function getRandomValue(){
-        $st_num=1;
-        $end_num=100;
-        $mul=1000000;
-
-        $randomNumber = mt_rand($st_num*$mul,$end_num*$mul)/$mul;
-        return round($randomNumber, 2);
     }
 
     function getBusiestDay($campus_id) {
