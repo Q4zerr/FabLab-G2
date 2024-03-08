f1faacf2bb15e5efc724678bb812696fae58339f
Luca Améaume <62576432+LuluxClient@users.noreply.github.com>
2024-03-08 17:32:13 +0100

    Started UpdateCurrentStudent live update

---
+++ assets/js/updateCurrentStudent.js
@@ -0,0 +1,22 @@
+// function updateCurrentStudent() {
+//     fetch("http://45.13.119.138:8000/total_etudiant/1")
+//             .then(response => response.json())
+//             .then(data => {
+//                     const studentCount = data.nombre_etudiant;
+//                     const chartElement = document.querySelector(".chart-level");
+//                     chartElement.style.height = `${studentCount}%`;
+//             })
+//             .catch(error => {
+//                     console.error("Erreur lors de la requête API :", error);
+//             });
+// }
+
+// // Met à jour toutes les 5 secondes (5000 millisecondes)
+// setInterval(updateCurrentStudent, 5000);
+
+// // Appelle la fonction pour mettre à jour la valeur dès le chargement de la page
+// updateCurrentStudent();
+
+
+
+// WIP merci de ne pas faire attention ce code est en cours de développement !!!!!!!!!!
